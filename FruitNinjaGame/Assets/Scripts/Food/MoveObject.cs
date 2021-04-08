6162282beae5155adec04f65d37e0728910bbe77
KirRya <kirillabduldjalilov@gmail.com>
2021-04-08 20:07:04 +0300

    Move and spawn
    Add temp and simple movement script and spawner, which generate food object and gave them sprite

---
+++ FruitNinjaGame/Assets/Scripts/Food/MoveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    public Vector2 startPosition;
+    public Vector2 endPosition;
+    public float step;
+    private float progress;
+
+
+
+    void Start()
+    {
+        transform.position = startPosition;
+        
+        
+    }
+
+
+    void Update() {
+        transform.position = Vector2.Lerp(startPosition, endPosition, progress);
+        progress += step;
+    }
+}
---
+++ FruitNinjaGame/Assets/Scripts/Spawner/ObjectSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject foodObject;
+
+    public float interval;
+
+    public Sprite[] sprites;
+
+
+    public Vector2 startPosition;
+    public Vector2 endPosition;
+    public float step;
+    private float progress;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startPosition;
+        InvokeRepeating("Spawn", interval, interval);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        GameObject food = Instantiate(foodObject);
+
+        transform.position = Vector2.Lerp(startPosition, endPosition, progress);
+        progress += step;
+
+        Sprite randomFoodSprite = sprites[Random.Range(0, sprites.Length)];
+        food.GetComponent<SpriteRenderer>().sprite = randomFoodSprite;
+
+    }
+}
