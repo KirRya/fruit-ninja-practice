cc668569ddc6adaeac34aa8fc7b344b963096e4c
KirRya <kirillabduldjalilov@gmail.com>
2021-04-18 00:10:39 +0300

    Popup

--- FruitNinjaGame/Assets/Scripts/UI/GameOverScript.cs
+++ FruitNinjaGame/Assets/Scripts/UI/GameOverScript.cs
@@ -13,17 +13,33 @@ public class GameOverScript : MonoBehaviour
     [SerializeField]
     public Text gameScoreText;
 
+    private Vector3 initScale;
+    private Vector3 endScale;
 
-    private float scaleIncrease = 25f;
+    [SerializeField]
+    private GameObject screen;
+
+
+    [SerializeField]
+    private SpriteRenderer heartImage;
+
+    [SerializeField]
+    private Sprite[] heartSprites;
 
     public void Setup() {
+        
+
         gameObject.SetActive(true);
+
+        heartImage.sprite = heartSprites[0];
+
+        endScale = screen.transform.localScale;
+        StartCoroutine(LerpScale());
         currScore.text = gameScoreText.text.Split(' ')[1];
     }
 
     public void RestartButton() {
         StartCoroutine(restartWithDelay());
-        
     }
 
     public void MainMenuButton() {
@@ -36,5 +52,21 @@ public class GameOverScript : MonoBehaviour
 
         SceneManager.LoadScene("Game");
         ParabolicMovement.isGameInProgress = true;
+        heartImage.sprite = heartSprites[1];
+    }
+
+
+    IEnumerator LerpScale()
+    {
+        float progress = 0;
+
+        while (progress <= 1)
+        {
+            screen.transform.localScale = Vector3.Lerp(Vector3.zero, endScale, progress);
+            progress += Time.deltaTime * Time.timeScale;
+            yield return null;
+        }
+
+        screen.transform.localScale = endScale;
     }
 }
